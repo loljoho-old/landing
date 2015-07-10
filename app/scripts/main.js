814167462ac4326c85f005fdc76ec6824405f111
loljoho <jho1290@smail.raritanval.edu>
2015-07-10 14:29:19 -0400

    Scripts files - work in progress, commented out

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+  console.log('Document Ready');
+
+/**
+ * WIP
+ */
+/*
+  var response = $.ajax({
+    method    : 'GET',
+    //url       : '../api/test.json',
+    url: 'test.json',
+    dataType  : 'json',
+    success   : function(results) {
+      console.log('Ajax GET success: ' + results);
+    }
+  });
+*/
+});
